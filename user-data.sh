c25ca4686d0161e4dd726265e3a50c87a0ab3ddf
cingco.cloud <50238769+GivenCingco@users.noreply.github.com>
2024-12-03 08:51:30 +0200

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,7 +1,22 @@
-                    #!/bin/bash
-                    sudo dnf update
-                    sudo dnf install -y postgresql16.x86_64 postgresql16-server
-                    sudo postgresql-setup --initdb
-                    sudo systemctl start postgresql
-                    sudo systemctl enable postgresql
-                    sudo systemctl status postgresql
+#!/bin/bash
+
+set -ex
+
+# Redirect all output to a log file for debugging
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+# Install dnf if it's not available
+sudo yum install -y dnf
+
+# Update system packages
+sudo dnf update -y
+
+# Install PostgreSQL
+sudo dnf install -y postgresql16.x86_64 postgresql16-server
+
+# Initialise the PostgreSQL database
+sudo /usr/pgsql-16/bin/postgresql-16-setup initdb
+
+# Start and enable PostgreSQL service
+sudo systemctl start postgresql-16
+sudo systemctl enable postgresql-16
